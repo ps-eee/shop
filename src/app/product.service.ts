31bef4397a68c193a412b4f48fe633bb97d75f93
Karmasakshi Goyal <karmasakshi@users.noreply.github.com>
2021-01-31 01:35:46 +0400

    Add Product service

---
+++ src/app/product.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Product } from './interfaces/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private products: Product[] = [
+    {
+      name: 'Nike Black Shoes',
+      id: 1,
+      imageUrls: { left: '/assets/1-left.jpg', bottom: '/assets/1-bottom.jpg', right: '/assets/1-right.jpg' }
+    },
+    {
+      name: 'Nike White Shoes',
+      id: 2,
+      imageUrls: { left: '/assets/2-left.jpg', bottom: '/assets/2-bottom.jpg', right: '/assets/2-right.jpg' }
+    }
+  ];
+
+  getAll(): Product[] {
+
+    return this.products;
+
+  }
+
+  getOne(productId: number): undefined | Product {
+
+    return this.products.find((product: Product): boolean => product.id === productId);
+
+  }
+
+}
