00d63eb124c2d0a6ec002b3838a68aaf9665827c
Karmasakshi Goyal <karmasakshi@users.noreply.github.com>
2021-01-31 11:37:50 +0400

    Add Product Page component

--- src/app/components/product-page/product-page.component.ts
+++ src/app/components/product-page/product-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/interfaces/product';
+import { Treatment } from 'src/app/interfaces/treatment';
+import { ProductService } from 'src/app/services/product/product.service';
+import { TreatmentService } from 'src/app/services/treatment/treatment.service';
 
 @Component({
   selector: 'shop-product-page',
@@ -7,9 +12,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductPageComponent implements OnInit {
 
-  constructor() { }
+  public product: undefined | Product = undefined;
+  public productHeroImgUrl: string = '';
+  public productImageUrls: string[] = [];
+  public treatment: undefined | Treatment = undefined;
 
-  ngOnInit(): void {
+  public constructor(
+    private activatedRoute: ActivatedRoute,
+    private productService: ProductService,
+    private treatmentService: TreatmentService
+  ) { }
+
+  public ngOnInit(): void {
+
+    const productId = Number(this.activatedRoute.snapshot.params.id);
+
+    this.product = this.productService.getOne(productId);
+
+    if (this.product !== undefined) {
+
+      this.productImageUrls = Object.values(this.product.imageUrls);
+
+      this.productHeroImgUrl = this.productImageUrls.length === 0 ? '' : this.productImageUrls[0];
+
+    }
+
+    this.treatmentService.treatment$.subscribe(
+
+      (treatment: Treatment) => {
+
+        this.treatment = treatment;
+
+        this.productHeroImgUrl = (this.product === undefined) ? '' : this.product.imageUrls[this.treatment.productHeroImage];
+
+      },
+
+      (error: Error): void => { console.log('treatment$ failed.', error); }
+
+    );
+
   }
 
 }
