ede07dc98b9f16e7cad361a6fdbfc2e68e24c543
Karmasakshi Goyal <karmasakshi@users.noreply.github.com>
2021-02-02 23:05:44 +0400

    Update interface

--- src/app/interfaces/exposure.ts
+++ src/app/interfaces/exposure.ts
@@ -2,7 +2,7 @@ import { TreatmentStatistic } from './treatment-statistic';
 import { User } from './user';
 
 export interface Exposure {
-  id: number;
+  id: string;
   isSuccessful: boolean;
   timestamp: string;
   treatmentHash: TreatmentStatistic['treatmentHash'];
